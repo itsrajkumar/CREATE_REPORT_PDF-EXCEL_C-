ba5cbf014ee7376c24cb405358c59a3605ae11ac
itsrajkumar <39757679+itsrajkumar@users.noreply.github.com>
2018-05-30 13:10:21 +0530

    Add files via upload

---
+++ GenerateExcel.cs
@@ -0,0 +1,274 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace DasTec.Factory.LycheeGST.Model.ExportExcel
+{
+    class GenerateExcel
+    {
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkBook;
+        Excel.Worksheet xlWorkSheet;
+        object misValue = System.Reflection.Missing.Value;
+        Excel.Range chartRange;
+        int worksheetCount = 1;
+
+        List<GenerateExcelData> data { get; set; }
+
+        public GenerateExcel(List<GenerateExcelData> data)
+        {
+            this.data = data;
+        }
+
+
+        public void getExcel()
+        {
+           // xlApp = new Excel.Application();
+           // xlWorkBook = xlApp.Workbooks.Add(misValue);
+
+            createWorksheet();
+
+          //  System.Windows.Forms.SaveFileDialog saveDlg = new System.Windows.Forms.SaveFileDialog();
+           // saveDlg.InitialDirectory = @"C:\";
+          //  saveDlg.Filter = "Excel files (*.xlsx)|*.xlsx";
+          //  saveDlg.FilterIndex = 0;
+           // saveDlg.RestoreDirectory = true;
+          //  saveDlg.Title = "Export Excel File To";
+          //  if (saveDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+          //  {
+           //     string path = saveDlg.FileName;
+            //    xlWorkBook.SaveCopyAs(path);
+             //   xlWorkBook.Saved = true;
+            //    xlWorkBook.Close(true, misValue, misValue);
+            //    xlApp.Quit();
+            //    MessageBox.Show("Excel file created and saved");
+           // }
+           // releaseObject(xlWorkSheet);
+           // releaseObject(xlWorkBook);
+           // releaseObject(xlApp);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+
+        void createWorksheet()
+        {
+            using (ExcelPackage objExcelPackage = new ExcelPackage())
+            {
+                  
+            foreach (GenerateExcelData d in data)
+            {
+               // if (worksheetCount > xlWorkBook.Worksheets.Count)
+              //  {
+              //      xlWorkBook.Worksheets.Add(After: xlWorkBook.Sheets[xlWorkBook.Sheets.Count]);
+            //    }
+               // xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(worksheetCount);
+              //  xlWorkSheet.Name = d.worksheetName;
+               // if(!String.IsNullOrEmpty(d.excelHeader))
+                 //   createHeader(d, "YELLOW", true, 10, "n");
+             //   mapData(d);
+                   worksheetCount++;
+
+                  //Create the worksheet    
+                        ExcelWorksheet objWorksheet = objExcelPackage.Workbook.Worksheets.Add(d.worksheetName);
+                        //Load the datatable into the sheet, starting from cell A1. Print the column names on row 1 
+
+                        objWorksheet.Name = d.worksheetName;
+                        // objWorksheet.Cells[1, 1] =dtSrc;
+                        objWorksheet.Cells["A1"].LoadFromDataTable(d.worksheetData, true);
+                        // objWorksheet.Cells.Style.Font.SetFromFont(new Font("Calibri", 10));    
+                       // objWorksheet.Cells.AutoFitColumns();    
+                        //Format the header   
+                        if (d.worksheetData.Columns[4].DataType.Name.ToString() != "Object" && d.worksheetData.Columns[4].DataType.Name.ToString() != "Int64")
+                        objWorksheet.Column(4).Style.Numberformat.Format = "yyyy-mm-dd";
+                        if (d.worksheetData.Columns[5].DataType.Name.ToString() != "Object" && d.worksheetData.Columns[5].DataType.Name.ToString() != "Int64")
+                        objWorksheet.Column(5).Style.Numberformat.Format = "yyyy-mm-dd";
+                        using (ExcelRange objRange = objWorksheet.Cells["A1:XFD1"])
+                        {
+                            objRange.Style.Font.Bold = true;
+                            objRange.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            objRange.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                           // objRange.Style.Fill.PatternType = ExcelFillStyle.LightUp;
+                            objRange.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                            objRange.Style.Fill.BackgroundColor.SetColor(Color.SkyBlue);
+                            //objRange.Style.Fill.BackgroundColor.SetColor(Color.FromA#eaeaea);    
+                        }
+
+                    
+                }
+           
+
+           // System.Windows.Forms.SaveFileDialog saveDlg = new System.Windows.Forms.SaveFileDialog();
+          //  saveDlg.InitialDirectory = @"C:\";
+          //  saveDlg.Filter = "Excel files (*.xlsx)|*.xlsx";
+          //  saveDlg.FilterIndex = 0;
+          //  saveDlg.RestoreDirectory = true;
+          //  saveDlg.Title = "Export Excel File To";
+          FileStream objFileStrm;
+          //  if (saveDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+           // {
+            //    string path = saveDlg.FileName;
+           //     if (File.Exists(path))
+             //       File.Delete(path);
+
+                //Create excel file on physical disk    
+             //   objFileStrm = File.Create(path);
+            //    objFileStrm.Close();
+
+                //Write content to excel file    
+             //   File.WriteAllBytes(path, objExcelPackage.GetAsByteArray());
+             //   MessageBox.Show("Excel file created and saved");
+                
+             //       System.Diagnostics.Process.Start(path);
+            //    
+           // }
+           // else 
+          //  {
+                     Random rnd = new Random();
+                     int randomNo = rnd.Next(1, 9999);
+                     string path = Path.GetTempPath() + "temp" + randomNo + ".xlsx";
+                if (File.Exists(path))
+                    File.Delete(path);
+                //Create excel file on physical disk    
+                objFileStrm = File.Create(path);
+                objFileStrm.Close();
+
+                //Write content to excel file    
+                File.WriteAllBytes(path, objExcelPackage.GetAsByteArray());
+
+                System.Diagnostics.Process.Start(path);
+
+          //  }
+
+
+
+            }
+
+         
+
+
+        }
+
+        void createHeader(GenerateExcelData d, string b, bool font, int size, string fcolor)
+        {
+            xlWorkSheet.Cells[1, 1] = d.excelHeader;
+            int unicode = 65 + d.worksheetData.Columns.Count-1;
+            char character = (char)unicode;
+            chartRange = xlWorkSheet.get_Range("A1", character.ToString()+"1");
+            chartRange.Merge(d.worksheetData.Columns.Count-1);
+            switch (b)
+            {
+                case "YELLOW":
+                    chartRange.Interior.Color = System.Drawing.Color.Yellow.ToArgb();
+                    break;
+                case "GRAY":
+                    chartRange.Interior.Color = System.Drawing.Color.Gray.ToArgb();
+                    break;
+                case "GAINSBORO":
+                    chartRange.Interior.Color =
+            System.Drawing.Color.Gainsboro.ToArgb();
+                    break;
+                case "Turquoise":
+                    chartRange.Interior.Color =
+            System.Drawing.Color.Turquoise.ToArgb();
+                    break;
+                case "PeachPuff":
+                    chartRange.Interior.Color =
+            System.Drawing.Color.PeachPuff.ToArgb();
+                    break;
+                default:
+                    //  workSheet_range.Interior.Color = System.Drawing.Color..ToArgb();
+                    break;
+            }
+
+            chartRange.Borders.Color = System.Drawing.Color.Black.ToArgb();
+            chartRange.Font.Bold = font;
+            chartRange.ColumnWidth = size;
+            if (fcolor.Equals(""))
+            {
+                chartRange.Font.Color = System.Drawing.Color.White.ToArgb();
+            }
+            else
+            {
+                chartRange.Font.Color = System.Drawing.Color.Black.ToArgb();
+            }    
+        
+        }
+
+        void mapData(GenerateExcelData d)
+        {
+            int j = 2;
+            int k = 1;
+            int colCount = d.worksheetData.Columns.Count;
+            int unicode = 65 + colCount - 1;
+            char character = (char)unicode;
+            
+
+            if (String.IsNullOrEmpty(d.excelHeader))
+                j = 1;
+            foreach (DataColumn col in d.worksheetData.Columns)
+            {
+
+                object cell = col.ToString();
+                xlWorkSheet.Cells[j, k++] = cell;
+                
+
+            }
+            chartRange = xlWorkSheet.get_Range("A" + j.ToString(), character.ToString() + j.ToString());
+
+            //var columnHeadingsRange = xlWorkSheet.Range[xlWorkSheet.Cells[j], xlWorkSheet.Cells[colCount]];
+            chartRange.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.LightPink);
+            chartRange.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Black);
+            chartRange.Font.Bold = true;
+           
+            //j = startPoint;
+            foreach (DataRow row in d.worksheetData.Rows)
+            {
+                ++j;
+                for (int i = 0; i < colCount; i++)
+                {
+                    object cell = row[i];
+                    xlWorkSheet.Cells[j, i + 1] = cell;
+                }
+
+            }
+            
+        }
+
+    }
+
+    public struct GenerateExcelData
+    {
+        public String excelHeader { get; set; }
+        public String worksheetName { get; set; }
+        public DataTable worksheetData { get; set; }
+        
+    }
+
+
+}
---
+++ PDFMaster.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Data;
+using System.IO;
+
+namespace DasTec.Factory.LycheeGST.Model
+{
+    class PDFMaster
+    {
+
+        public PDFMaster()
+        {
+
+        }
+        
+        public void createPDF(String strHeader, Dictionary<string, string> dict,String FromDate,String EndDate, DataTable dt) 
+        {
+
+
+            Random rnd = new Random();
+            int randomNo = rnd.Next(1, 9999);
+            string path = Path.GetTempPath() + "tempPdf" + randomNo + ".pdf";
+            if (File.Exists(path))
+                File.Delete(path);
+            System.IO.FileStream fs = new FileStream(path, FileMode.Create);
+            // Create an instance of the document class which represents the PDF document itself.
+            Document document = new Document(PageSize.A4, 25, 25, 30, 30);
+
+            // Create an instance to the PDF file by creating an instance of the PDF 
+            // Writer class using the document and the filestrem in the constructor.
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+
+            PdfPCell cell, cell1, cell2, cell3;  
+
+            // Add meta information to the document
+            document.AddAuthor("DasTec");
+            document.AddCreator("Supported By DasTec Solution pvt. Ltd.");
+            document.AddKeywords("PDF Master");
+            document.AddSubject("Document subject - Describing the steps creating a PDF document");
+            document.AddTitle(strHeader);
+
+            // Open the document to enable you to write to the document
+            document.Open();
+
+            PdfPTable pt = new PdfPTable(1);
+            PdfPTable headert = new PdfPTable(2);
+
+
+            //Report Header
+            BaseFont bfntHead = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font fntHead = new Font(bfntHead, 15, 1, BaseColor.GRAY);
+            Paragraph prgHeading = new Paragraph();
+            prgHeading.Alignment = Element.ALIGN_CENTER;
+            prgHeading.Add(new Chunk(strHeader.ToUpper(), fntHead));
+            document.Add(prgHeading);
+
+            //Author
+            Paragraph prgAuthor = new Paragraph();
+            BaseFont btnAuthor = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font fntAuthor = new Font(btnAuthor,10, 2, BaseColor.GRAY);
+            prgAuthor.Alignment = Element.ALIGN_CENTER;
+
+            for (int i = 0; i < dict.Count; i++)
+            {
+               /* Console.WriteLine("Key: {0}, Value: {1}",
+                                                        dict.Keys.ElementAt(i),
+                                                        dict[dict.Keys.ElementAt(i)]);*/
+
+                prgAuthor.Add(new Chunk(dict.Keys.ElementAt(i)+" : "+ dict[dict.Keys.ElementAt(i)]+"\n", fntAuthor));
+                
+            }
+            prgAuthor.Add(new Chunk("Date : " + FromDate+" TO "+EndDate, fntAuthor));
+
+            document.Add(prgAuthor);
+
+            //Add a line seperation
+            Paragraph p = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 1)));
+            document.Add(p);
+
+            //Add line break
+            document.Add(new Chunk("\n", fntHead));
+
+            //Write the table
+            PdfPTable table = new PdfPTable(dt.Columns.Count);
+            table.WidthPercentage = 100;
+
+            if (dt.Columns.Count == 6)
+            {
+                var colWidthPercentages = new[] { 15f, 20f, 35f, 10f, 10f, 10f };
+                table.SetWidths(colWidthPercentages);
+            }
+            //Table header
+            BaseFont btnColumnHeader = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+          
+            Font fntColumnHeader = new Font(btnColumnHeader, 8, 1, BaseColor.WHITE);
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                cell = new PdfPCell();
+                cell.BackgroundColor = BaseColor.GRAY;
+                cell.AddElement(new Chunk(dt.Columns[i].ColumnName.ToUpper(), fntColumnHeader));
+                table.AddCell(cell);
+            }
+            //table Data
+            Font fntColumnDetail = new Font(btnColumnHeader, 7, 1, BaseColor.BLACK);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    cell = new PdfPCell();
+                    cell.BackgroundColor = BaseColor.WHITE;
+                    cell.AddElement(new Chunk(dt.Rows[i][j].ToString(), fntColumnDetail));
+                    table.AddCell(cell);
+                    //table.AddCell(dt.Rows[i][j].ToString());
+                }
+            }
+
+            document.Add(table);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            pt.WidthPercentage = 100;
+            float[] width = { 100f };
+            pt.SetWidths(width);
+            pt.SpacingAfter = 0;
+            pt.SpacingBefore = 0;
+
+            //OnEndPage(writer, document);
+
+            cell2 = new PdfPCell(new Phrase(" *** This s a system generated Invoice *** ", fntColumnDetail));
+            cell2.HorizontalAlignment = Element.ALIGN_CENTER;
+            //cell2.Colspan = 14;
+            cell2.Border = 0;
+            cell2.Padding = 0;
+            pt.AddCell(cell2);
+            document.Add(pt);
+
+            
+            // Close the document
+            document.Close();
+            // Close the writer instance
+            writer.Close();
+            // Always close open filehandles explicity
+            fs.Close();
+
+
+
+
+
+
+
+            System.Diagnostics.Process.Start(path);
+
+        }
+
+
+        public void OnEndPage(PdfWriter writer, Document document)
+        {
+            //base.OnEndPage(writer, document);
+
+            var content = writer.DirectContent;
+            var pageBorderRect = new Rectangle(document.PageSize);
+
+            pageBorderRect.Left += document.LeftMargin;
+            pageBorderRect.Right -= document.RightMargin;
+            pageBorderRect.Top -= document.TopMargin;
+            pageBorderRect.Bottom += document.BottomMargin;
+
+            content.SetColorStroke(BaseColor.BLACK);
+            content.Rectangle(pageBorderRect.Left, pageBorderRect.Bottom, pageBorderRect.Width, pageBorderRect.Height);
+            content.Stroke();
+        }
+    }
+}
